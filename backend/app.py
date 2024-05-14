8171614eb19aacfccbe8d3cb0b5c4de1f1d24d92
Dominika Jelińska <dominikajelinska82@gmail.com>
2024-05-15 00:18:00 +0200

    Poprawa zwracania rekomendacji - zwrócenie id produktów

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,11 @@
 from backend.models.knn_model import KNNModel
-from backend.utils.data_processing import load_data, preprocess_data
+from backend.utils.recommendation import get_top_recommendations
 
-products = load_data("./data/sephora_products_dataset.csv")
-reviews = load_data("./data/sephora_reviews_dataset.csv")
-
-features_df = preprocess_data(products, reviews)
-print(features_df)
+knn_model = KNNModel()
 
 product_id = "P422510"
 k_recommendations = 6
 
-knn_model = KNNModel()
-knn_model.train(features_df)
-neighbor_indices = knn_model.get_neighbors(features_df, product_id, k_recommendations)
+recommendations = get_top_recommendations(product_id, knn_model, k_recommendations)
 
-print(neighbor_indices)
+print(recommendations)
---
+++ backend/utils/recommendation.py
@@ -0,0 +1,21 @@
+from backend.utils.data_processing import load_data, preprocess_data
+
+
+def get_top_recommendations(product_id, knn_model, k=3):
+    products = load_data("./data/sephora_products_dataset.csv")
+    reviews = load_data("./data/sephora_reviews_dataset.csv")
+
+    features_df = preprocess_data(products, reviews)
+
+    knn_model.train(features_df)
+
+    neighbor_indices = knn_model.get_neighbors(features_df, product_id, k)
+
+    recommendations = []
+    for i in range(0, k):
+        neighbor_index = neighbor_indices.flatten()[i]
+        neighbor_product_id = features_df.index[neighbor_index]
+
+        recommendations.append(neighbor_product_id)
+
+    return recommendations
